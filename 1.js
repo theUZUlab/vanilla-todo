3a39185621e947dfe29a48eb5cceeda9a0c3109d
Jang Yuju <ujoo2802@gmail.com>
2024-07-21 17:05:18 +0900

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,37 @@
+const todoList = document.querySelector('ul');
+const resultText = document.querySelector('input');
+const resultBtn = document.querySelector('#resultBtn');
+
+resultBtn.addEventListener('click', (e) => {
+    if (resultText.value.trim() === '') {
+        console.log('빈 값 들어가서 생성 취소');
+        resultText.value = '';
+        return;
+    }
+
+    const newLi = document.createElement('li');
+    const todoText = document.createTextNode(resultText.value);
+    newLi.appendChild(todoText);
+    todoList.appendChild(newLi);
+    console.log('li 생성');
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'X';
+    newLi.appendChild(delBtn);
+    console.log('삭제 버튼 생성');
+
+    resultText.value = '';
+});
+
+todoList.addEventListener('click', (e) => {
+    let delResult = false;
+
+    if (e.target.tagName === 'BUTTON') {
+        delResult = confirm('정말로 삭제하시겠습니까?');
+        console.log('삭제하실?');
+    }
+    if (delResult === true) {
+        e.target.parentElement.remove();
+        console.log('ㅇㅇ 나 삭제할래');
+    }
+});
